a47d6f25d39d035d60d9fa351bd6e151417c010d
c-tel <stas.telizhenko@gmail.com>
2019-04-25 02:19:34 +0300

    комета з авторизацією

--- db/students-dao.js
+++ db/students-dao.js
@@ -10,5 +10,23 @@ module.exports = {
 			return {success: false, err: e};
 		}
 	},
+	getByAccID: async (acc_id) => {
+		const client = await pool.connect();
+		try {
+			const result = await client.query("SELECT * FROM student WHERE acc_id = $1", [acc_id]);
+			return {success: true, data: result.rowCount !== 0 ? result.rows[0] : null};
+		} catch (e) {
+			return {success: false, err: e};
+		}
+	},
+	getStudentsSpecializationByID: async(id) => {
+		const client = await pool.connect();
+		try {
+			const result = await client.query("SELECT * FROM student WHERE acc_id = $1", [acc_id]);
+			return {success: true, data: result.rowCount !== 0 ? result.rows[0] : null};
+		} catch (e) {
+			return {success: false, err: e};
+		}
+	}
 
 };
