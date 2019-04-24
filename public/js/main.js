d737496a94d470f197f6b1bcd95590ef5b0d95e9
Maksym <maxim4110@gmail.com>
2019-04-24 20:05:51 +0300

    login page stylized navbar and hero sections done

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,33 @@
 $(function () {
-    // const navCollapse = 767;
+    const $nav = $('nav')
     const $mainDiv = $('#main-div')
 
     function addNavbarPadding () {
-        $mainDiv.css('padding-top', $('nav').outerHeight())
+        if ($nav.hasClass('transparent'))
+            return
+        $mainDiv.css('padding-top', $nav.outerHeight())
     }
 
     function addFooterPadding () {
         $mainDiv.css('padding-bottom', $('footer').outerHeight())
     }
 
-    $(window).on('resize', addFooterPadding)
+
+    function checkScrolled () {
+        if ($(window).scrollTop() > 15) {
+            $nav.addClass('scrolled')
+        } else {
+            $nav.removeClass('scrolled')
+        }
+    }
 
     addNavbarPadding()
     addFooterPadding()
+    checkScrolled()
+
+    $(window).on('resize', function () {
+        addNavbarPadding()
+        addFooterPadding()
+    })
+    $(window).on('scroll', checkScrolled)
 })
