35d3b17a7f1d990cd226d282ce90971fac6738e3
Maksym <maxim4110@gmail.com>
2019-04-26 00:53:52 +0300

    cometa hz chto tut

--- routes/course.js
+++ routes/course.js
@@ -1,8 +1,15 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const subjects = require('../db/subject-dao')
 
-router.get("/:id", function (req, res, next) {
-	const course_id = req.params.id
+router.get("/:id", async function (req, res, next) {
+	// const course_id = req.params.id
+	// let result;
+	// try {
+	// 	result = await subjects.getSubjectById(course_id)
+	// } catch (e) {
+	// 	result = []
+	// }
 	res.render("pages/course", {course: require("../db/_testing_obj").courseExample});
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,24 @@
-"use strict"
-const express = require("express")
+'use strict'
+const express = require('express')
 const router = express.Router()
-const subjects = require("../db/subject-dao")
-const students = require("../db/students-dao")
+const subjects = require('../db/subject-dao')
+const students = require('../db/students-dao')
 
 /* GET home page. */
-router.get("/", async (req, res, next) => {
-	let filterArgs = req.query
-	if (res.locals.user.role === "student") {
-		filterArgs["studentID"] = res.locals.user.model.id
-	} else {
-		delete filterArgs["studentID"]
-	}
-	let results = []
-	try {
-		results = await subjects.getFilteredSubjects(req.query)
-	} catch (e) {
-		results = []
-	}
-	res.render("pages/index", {courses: results.data, filters: filterArgs})
+router.get('/', async (req, res, next) => {
+    let filterArgs = req.query
+    if (res.locals.user.role === 'student') {
+        filterArgs['studentID'] = res.locals.user.model.id
+    } else {
+        delete filterArgs['studentID']
+    }
+    let results = []
+    try {
+        results = await subjects.getFilteredSubjects(req.query)
+    } catch (e) {
+        results = []
+    }
+    res.render('pages/index', {courses: results.data, filters: filterArgs})
 })
 
 module.exports = router
