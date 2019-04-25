6a6525c104063ecb460c258866802c9bf51fa105
TheGreatNectarine <margal.nik@gmail.com>
2019-04-26 02:41:01 +0300

    перевірка на можливість редагування

--- db/students-dao.js
+++ db/students-dao.js
@@ -1,16 +1,23 @@
-const pool = require('./db-pool');
+const pool = require("./db-pool")
 
 module.exports = {
 	getByID: async (id) => {
-		return await pool.fetchOne("SELECT * FROM student WHERE id = $1", [id]);
+		return await pool.fetchOne("SELECT * FROM student WHERE id = $1", [id])
 	},
 	getByAccID: async (acc_id) => {
-		return await pool.fetchOne("SELECT * FROM student WHERE acc_id = $1", [acc_id]);
+		return await pool.fetchOne("SELECT * FROM student WHERE acc_id = $1", [acc_id])
 	},
-	getStudentsSpecializationByID: async(id) => {
+	getStudentsSpecializationByID: async (id) => {
 		return await pool.fetchOne(`SELECT * FROM specialization 
 		WHERE id = 	(SELECT specialization_id FROM student
-					WHERE student.id = ${id});`);
+					WHERE student.id = ${id});`)
+	},
+	studentWithIDCanReviewSubjectWithID: async (studentID, subjectID) => {
+		return await pool.fetchOne(`
+          SELECT EXISTS(SELECT *
+                        FROM student_subject ss
+                        WHERE ss.student_id = $1
+                          AND ss.subject_id = $2)
+		`, [studentID, subjectID])
 	}
-
-};
+}
