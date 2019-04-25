8c5f58b086ad7e64976b9062c0ba04a5dfc9be9a
TheGreatNectarine <margal.nik@gmail.com>
2019-04-25 03:32:45 +0300

    комета фільтри 4

--- routes/subjects-page.js
+++ routes/subjects-page.js
@@ -6,17 +6,21 @@ const students = require("../db/students-dao");
 
 router.get("/", async (req, res, next) => {
 	const filterArgs = req.query;
-	console.log(req.user.model);
-	const results = await subjects.getFilteredSubjects({
-		faculty: "Факультет інформатики",
-		tags: ["Факультет інформатики", "Програмування", "C++"],
-		credits: 3,
-		trimester: "2Д",
-		specialization: "students.getStudentsSpecializationByID()",
-		subject_type: "Професійно-орієнтований"
-		//знайти проф-орієнтовані дисципліни для студента з айді
-	});
-	res.send({results: results, filters: filterArgs});
+	// console.log(req.user.model);
+	try {
+		const results = await subjects.getFilteredSubjects({
+			faculty: "Факультет інформатики",
+			tags: ["Факультет інформатики", "Програмування", "C++"],
+			credits: 3,
+			trimester: "2Д",
+			specialization: "students.getStudentsSpecializationByID()",
+			subject_type: "Професійно-орієнтований"
+			//знайти проф-орієнтовані дисципліни для студента з айді
+		});
+		res.send({results: results, filters: filterArgs});
+	} catch (e) {
+		res.send({results: [], filters: filterArgs, err: e});
+	}
 });
 
 module.exports = router;
