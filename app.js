cc99c2f5cb92a9d88fa639c1148f8fed8df0331f
TheGreatNectarine <margal.nik@gmail.com>
2019-04-25 23:38:32 +0300

    створення предметів

--- app.js
+++ app.js
@@ -1,69 +1,70 @@
-require("dotenv").config();
-const sessions = require('./middleware/sessions');
+require("dotenv").config()
+const sessions = require("./middleware/sessions")
 
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+const createError = require("http-errors")
+const express = require("express")
+const path = require("path")
+const cookieParser = require("cookie-parser")
+const logger = require("morgan")
 
-const app = express();
+const app = express()
 
 const setUpMiddleware = () => {
-	app.use(sessions.sessionMiddleware);
-};
+	app.use(sessions.sessionMiddleware)
+}
 
 const setUpRoutes = () => {
-	app.use(`/`, require("./routes/index"));
-	app.use(`/login`, require("./routes/login"));
-	app.use(`/profile`, require("./routes/profile"));
-	app.use(`/course`, require("./routes/course"));
-	app.use(`api/v1/students`, require("./api/studentsRouter"));
-	app.use(`api/v1/teachers`, require("./api/teachersRouter"));
-	app.use(`api/v1/subjects`, require("./api/subjectsRouter"));
-	app.use(`api/v1/tags`, require("./api/tagsRouter"));
-	app.use(`api/v1/faculties`, require("./api/facultiesRouter"));
-	app.use(`api/v1/specializations`, require("./api/specializationsRouter"));
-	app.use(`api/v1/chairs`, require("./api/chairsRouter"));
-	app.use(`/subjects`, require("./routes/subjects-page"));
-	app.use(`/api/v1/users`, require("./api/users"));
-};
+	app.use(`/`, require("./routes/index"))
+	app.use(`/login`, require("./routes/login"))
+	app.use(`/profile`, require("./routes/profile"))
+	app.use(`/course`, require("./routes/course"))
+	app.use(`api/v1/students`, require("./api/studentsRouter"))
+	app.use(`api/v1/teachers`, require("./api/teachersRouter"))
+	app.use(`api/v1/subjects`, require("./api/subjectsRouter"))
+	app.use(`api/v1/tags`, require("./api/tagsRouter"))
+	app.use(`api/v1/faculties`, require("./api/facultiesRouter"))
+	app.use(`api/v1/specializations`, require("./api/specializationsRouter"))
+	app.use(`api/v1/chairs`, require("./api/chairsRouter"))
+	// app.use(`/subjects`, require("./routes/_subjects-page"));
+	app.use(`/subjects`, require("./routes/subjects"))
+	app.use(`/api/v1/users`, require("./api/users"))
+}
 
 const setUpViews = () => {
-	app.set("views", path.join(__dirname, "views"));
-	app.set("view engine", "ejs");
-};
+	app.set("views", path.join(__dirname, "views"))
+	app.set("view engine", "ejs")
+}
 
 const setUpModules = () => {
-	app.use(logger("dev"));
-	app.use(express.json());
-	app.use(express.urlencoded({extended: false}));
-	app.use(cookieParser());
-	app.use(express.static(path.join(__dirname, "public")));
-};
+	app.use(logger("dev"))
+	app.use(express.json())
+	app.use(express.urlencoded({extended: false}))
+	app.use(cookieParser())
+	app.use(express.static(path.join(__dirname, "public")))
+}
 
 const setUpErrorHandlers = () => {
 	// catch 404 and forward to error handler
 	app.use(function (req, res, next) {
-		next(createError(404));
-	});
+		next(createError(404))
+	})
 
 // error handler
 	app.use(function (err, req, res, next) {
 		// set locals, only providing error in development
-		res.locals.message = err.message;
-		res.locals.error = req.app.get("env") === "development" ? err : {};
+		res.locals.message = err.message
+		res.locals.error = req.app.get("env") === "development" ? err : {}
 
 		// render the error page
-		res.status(err.status || 500);
-		res.render("pages/error");
-	});
-};
+		res.status(err.status || 500)
+		res.render("pages/error")
+	})
+}
 
-setUpModules();
-setUpMiddleware();
-setUpRoutes();
-setUpViews();
-setUpErrorHandlers();
+setUpModules()
+setUpMiddleware()
+setUpRoutes()
+setUpViews()
+setUpErrorHandlers()
 
-module.exports = app;
+module.exports = app
--- db/db-pool.js
+++ db/db-pool.js
@@ -1,4 +1,4 @@
-const Pool = require("pg").Pool;
+const Pool = require("pg").Pool
 const pool = new Pool({
 	user: "zhckvycszhbgeh",
 	host: "ec2-54-195-252-243.eu-west-1.compute.amazonaws.com",
@@ -6,37 +6,50 @@ const pool = new Pool({
 	password: "ef4e69d20ee11a1fc24a00adae701c4ac2cd16e13b8a29247ab3b145a339dae6",
 	port: 5432,
 	ssl: true
-});
+})
+
+module.exports.execute = async (query, params = []) => {
+	const client = await pool.connect()
+	try {
+		await client.query(query, params)
+		return {success: true, params: params}
+	} catch (e) {
+		console.log(e)
+		return {success: false, err: e}
+	} finally {
+		client.release()
+	}
+}
 
 module.exports.fetchOne = async (query, params = []) => {
-	const client = await pool.connect();
+	const client = await pool.connect()
 	try {
-		const result = await client.query(query, params);
-		return {success: true, data: result.rowCount !== 0 ? result.rows[0] : null};
+		const result = await client.query(query, params)
+		return {success: true, data: result.rowCount !== 0 ? result.rows[0] : null}
 	} catch (e) {
-		return {success: false, err: e};
+		return {success: false, err: e}
 	} finally {
-		client.release();
+		client.release()
 	}
-};
+}
 
 module.exports.fetchMany = async (query, params = []) => {
-	const client = await pool.connect();
+	const client = await pool.connect()
 	try {
-		const result = await client.query(query, params);
-		return {success: true, data: result !== null ? result.rows : []};
+		const result = await client.query(query, params)
+		return {success: true, data: result !== null ? result.rows : []}
 	} catch (e) {
-		return {success: false, err: e};
+		return {success: false, err: e}
 	} finally {
-		client.release();
+		client.release()
 	}
-};
+}
 
 module.exports.fetchScalar = async (query, attribute, params = []) => {
-	const row = await module.exports.fetchOne(query, params);
+	const row = await module.exports.fetchOne(query, params)
 	if (row.success) {
-		return row["data"][attribute];
+		return row["data"][attribute]
 	} else {
-		return null;
+		return null
 	}
-};
+}
