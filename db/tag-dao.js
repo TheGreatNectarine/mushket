5ab22d5c68eb02eec39951f5b28885a20a365b85
TheGreatNectarine <margal.nik@gmail.com>
2019-04-26 11:06:33 +0300

    кометазін тегі

--- db/tag-dao.js
+++ db/tag-dao.js
@@ -1,6 +1,18 @@
-"use strict";
+"use strict"
 const pool = require("./db-pool")
 
 module.exports = {
-	getAllTags: async () => await pool.fetchMany("SELECT * FROM tag")
+	getAllTags: async () => await pool.fetchMany("SELECT * FROM tag"),
+	getSelectedTags: async (id) => {
+		const query = `
+          SELECT t.id,
+                 t.name,
+                 (EXISTS(SELECT * FROM student_tag st WHERE st.student_id = s.id AND st.tag_id = t.id)) AS SELECTED
+          FROM tag t,
+               student s
+		  WHERE s.id = $1
+		`
+		const args = [id]
+		return await pool.fetchMany(query, args)
+	}
 }
