11c1f2b36ee761974823d64f2262d8a1f95e359b
TheGreatNectarine <margal.nik@gmail.com>
2019-04-26 10:51:02 +0300

    комета предмети студента і тічера

--- db/students-dao.js
+++ db/students-dao.js
@@ -16,13 +16,26 @@ module.exports = {
 		const result = await pool.fetchOne(`
           SELECT EXISTS(SELECT *
                         FROM student_subject ss
-                        WHERE ss.student_id = $1
-                          AND ss.subject_id = $2)
+                        WHERE ss.student_id = $ 1
+                          AND ss.subject_id = $ 2)
 		`, [studentID, subjectID])
 		if (result.data && result.data.exists) {
 			return result.data.exists
 		} else {
 			return false
 		}
+	},
+	getSubjectsByStudentID: async (id) => {
+		const query = `
+          SELECT *
+          FROM subject sub
+          WHERE sub.id IN (SELECT s.id
+                           FROM student st
+                                  INNER JOIN student_subject ss on st.id = ss.student_id
+                                  INNER JOIN subject s on ss.subject_id = s.id
+                           WHERE st.id = $1)
+		`
+		const args = [id]
+		return await pool.fetchMany(query, args)
 	}
 }
--- db/teachers-dao.js
+++ db/teachers-dao.js
@@ -7,4 +7,17 @@ module.exports = {
     getByID: async (id) => {
         return await pool.fetchOne("SELECT * FROM teacher WHERE id = $1", [id])
     },
+    getSubjectsByTeacherID: async (id) => {
+        const query = `
+        SELECT *
+        FROM subject sub
+        WHERE sub.id IN (SELECT DISTINCT s.id
+                         FROM teacher t 
+                         INNER JOIN teacher_subject ts on t.id = ts.teacher_id
+                         INNER JOIN subject s on ts.subject_id = s.id
+                         WHERE t.id = $1)
+        `
+        const args = [id]
+        return await pool.fetchMany(query, args)
+    },
 }
